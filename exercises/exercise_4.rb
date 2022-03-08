5af06f9fedb0e4aebbf6a126b17fe8d07b23a019
Pouya Elishaee <p.elishaee@gmail.com>
2022-03-08 02:42:42 +0000

    done

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,27 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: "Surrey", 
+  annual_revenue: 224000, 
+  mens_apparel:false, 
+  womens_apparel: true)
+
+Store.create(name: "Whistler", 
+  annual_revenue: 1900000, 
+  mens_apparel:true, 
+  womens_apparel: false)
+
+Store.create(name: "Yaletown", 
+  annual_revenue: 430000, 
+  mens_apparel:true, 
+  womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+
+# Loop through each of these stores and output their name and annual revenue on each line.
+@mens_stores.each {|x| puts "name: #{x.name}, annual_revenue: #{x.annual_revenue}"}
+
+# Do another fetch but this time load stores that carry women's apparel and are generating less than $1M in annual revenue.
+@womens_stores = Store.where(["womens_apparel = ? and annual_revenue < ?", "true", 1000000])
+
+@womens_stores.each {|x| puts "name: #{x.name}, annual_revenue: #{x.annual_revenue}"}
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find_by(id: 1)
+@store1.employees.create(first_name: "POOYA", last_name: "EL", hourly_rate: 60)
+@store1.employees.create(first_name: "SAM", last_name: "HI", hourly_rate: 10)
+@store1.employees.create(first_name: "NOEL", last_name: "LI", hourly_rate: 40)
+
+@store1.employees.each {|x| puts "#{x.first_name}"}
